f651aeff78d489f2a5ca0663d9045b468a066196
Leelabari Fulbel <fulbel.leelabari@gmail.com>
2017-02-28 17:35:04 -0600

    stuff

--- index.php
+++ index.php
@@ -1,27 +1,40 @@
 <?php
-  echo "<html>";
+  echo "<html
+        <head>
+           <script src=\"http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js\"></script>
+           <title>Matt and Lee's Project 5</title>
+       </head>
+          <body>";
  ?>
-     <head>
-         <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
-         <title>Matt and Lee's Project 5</title>
-     </head>
 
-     <body>
-
-         <form id="loginMenu">
-             <h1>Login</h1>
-             User Name:<br><input type="text" id="username"><br><br>
-             Password:<br><input type="text" id="password"><br><br>
-             <input type="button" id="login-button" value="Login">
-         </form>
+  <form id="signUpMenu">
+     <h1>SignUp</h1>
+     User Name:<br><input type="text" id="username"><br><br>
+     Password:<br><input type="text" id="password"><br><br>
+     <input type="button" id="signUp-button" value="signUp">
+  </form>
 
 
+  <form id="loginMenu" style="display:none;">
+     <h1>Login</h1>
+     User Name:<br><input type="text" id="username"><br><br>
+     Password:<br><input type="text" id="password"><br><br>
+     <input type="button" id="login-button" value="Login">
+  </form>
 
-     </body>
 
-     <script src="actions.js"></script>
+  <form id="loginMenu" style="display:none;">
+     <h1>Make a Post</h1>
+     Post Title:<br><input type="text" id="post"><br>
+     <input type="button" id="login-button" value="Login">
+  </form>
 
 
 
 
-<?php  echo "</html>" ?>
+
+
+<?php  echo " </body>
+ <script src=\"actions.js\"></script>
+ </html>"
+?>
