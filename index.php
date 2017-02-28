3c1d7a03aed34405cd7e13ac1b420c44e735d891
Leelabari Fulbel <fulbel.leelabari@gmail.com>
2017-02-28 17:08:57 -0600

    stuff

--- index.php
+++ index.php
@@ -1,7 +1,27 @@
 <?php
-
-
-  echo "hello";
-
-
+  echo "<html>";
  ?>
+     <head>
+         <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+         <title>Matt and Lee's Project 5</title>
+     </head>
+
+     <body>
+
+         <form id="loginMenu">
+             <h1>Login</h1>
+             User Name:<br><input type="text" id="username"><br><br>
+             Password:<br><input type="text" id="password"><br><br>
+             <input type="button" id="login-button" value="Login">
+         </form>
+
+
+
+     </body>
+
+     <script src="actions.js"></script>
+
+
+
+
+<?php  </html>?>
