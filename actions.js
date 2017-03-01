6c227421f01ade5a4841afa11f74e08d19237a55
Leelabari Fulbel <fulbel.leelabari@gmail.com>
2017-02-28 18:01:01 -0600

    stuff

--- actions.js
+++ actions.js
@@ -3,3 +3,24 @@
 $( document ).ready(function() {
     console.log( "ready!" );
 });
+
+
+// function signUp(){
+//
+//   var info = {username: $("#username").html(), password: $("#password").html() };
+//
+//   $.ajax({
+//       url: 'signUp.php',
+//       type: "POST",
+//       data: info,
+//       success: function(data) {
+//
+//       },
+//       error: function(data) {
+//           console.log(data);
+//
+//       }
+//   });
+//
+//
+// }
--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+  session_start();
   echo "<html
         <head>
            <script src=\"http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js\"></script>
@@ -7,27 +8,54 @@
           <body>";
  ?>
 
-  <form id="signUpMenu">
+
+  <form id="signUpMenu" >
      <h1>SignUp</h1>
-     User Name:<br><input type="text" id="username"><br><br>
-     Password:<br><input type="text" id="password"><br><br>
+     User Name:<br><input type="text" id="username" name="username"><br><br>
+     Password:<br><input type="text" id="password" name="password"><br><br>
      <input type="button" id="signUp-button" value="signUp">
   </form>
+
+  <div id="exPost0">
+    <h2>Example Post Look</h2>
+    <p>Post Title:</p>
+    <input type="text" id="exPostTitle0">
+    <p>
+    <label>Text Area</label><br>
+    <textarea id = "myTextArea" rows = "3"  cols = "80">Your text here</textarea>
+    </p>
+  </div>
 
 
   <form id="loginMenu" style="display:none;">
      <h1>Login</h1>
-     User Name:<br><input type="text" id="username"><br><br>
-     Password:<br><input type="text" id="password"><br><br>
+     User Name:<br><input type="text" id="username" name="username"><br><br>
+     Password:<br><input type="text" id="password" name="password"><br><br>
      <input type="button" id="login-button" value="Login">
   </form>
 
 
-  <form id="loginMenu" style="display:none;">
-     <h1>Make a Post</h1>
-     Post Title:<br><input type="text" id="post"><br>
-     <input type="button" id="login-button" value="Login">
-  </form>
+  <div id="viewPosts" style="display:none;">
+
+    <div id="allPost"></div>
+
+
+
+
+
+
+    <form id="makeAPost" style="display:none;">
+       <h1>Make a Post</h1>
+       Post Title:<br><input type="text" id="post"><br>
+       <label>Text Area</label>
+          <textarea> id = "myTextArea"
+                  rows = "3"
+                  cols = "80">Your text here</textarea>
+       <input type="button" id="login-button" value="Login">
+    </form>
+
+  </div>
+
 
 
 
